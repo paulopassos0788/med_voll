311f67ae70a1d17b3157bf1bf29774d2b6e8d362
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-03-21 10:31:33 -0300

    implementando CRUD do paciente

---
+++ src/main/java/br/com/passos/med_voll/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package br.com.passos.med_voll.paciente;
+
+import br.com.passos.med_voll.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+}
---
+++ src/main/java/br/com/passos/med_voll/paciente/Paciente.java
@@ -0,0 +1,120 @@
+package br.com.passos.med_voll.paciente;
+
+import br.com.passos.med_voll.endereco.Endereco;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pacientes")
+public class Paciente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    @Embedded
+    private Endereco endereco;
+    private Boolean ativo;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+    public Paciente(){
+
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Paciente paciente = (Paciente) o;
+        return Objects.equals(id, paciente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
